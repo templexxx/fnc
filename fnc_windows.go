422365cdd027f6f2434b9387726fad946df2a273
templexxx <lucas1x1x@gmail.com>
2020-12-04 10:11:12 +0800

    add windows supports
    actually fnc still only work welll on linux, but sometime we want to run other unit test which imports fnc on windows.

---
+++ fnc_windows.go
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2019. Temple3x (temple3x@gmail.com)
+ *
+ * Use of this source code is governed by the MIT License
+ * that can be found in the LICENSE file.
+ */
+
+package fnc
+
+import "os"
+
+// Disable all File access time(atime) updates,
+// darwin doesn't have it.
+const O_NOATIME = 0
+
+func syncRange(f *os.File, off int64, n int64, flags int) (err error) {
+
+	return f.Sync()
+}
+
+func fadvise(f *os.File, offset, size int64, advice int) (err error) {
+	return
+}
+
+func preAllocate(f *os.File, size int64) error {
+	return f.Truncate(size)
+}
